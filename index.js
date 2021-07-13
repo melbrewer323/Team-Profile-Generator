6772a260b62b6ad228e13babf8113a177cbce32a
Melissa Brewer <melramirez727@gmail.com>
2021-07-12 21:55:04 -0500

    revised empl array

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const Manager = require("./team/Manager");
 const Engineer = require("./team/Engineer");
 const Intern = require("./team/Intern");
-const employeeArray = []
+const managerArray = []
+const engineerArray = []
+const internArray = []
 /* THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
 WHEN I select the engineer option
@@ -87,7 +89,7 @@ const managerQuestions = () => {
         .then((answers) => {
             const addManager =  new Manager(answers.name, answers.id, answers.email, answers.officeNum);
             employeeArray.push(addManager);
-            console.log(employeeArray);   
+            console.log(managerArray);   
 
             firstQuestions()
 
@@ -132,7 +134,7 @@ const managerQuestions = () => {
         ])
         .then((answers) => {
             const addEngineer =  new Engineer(answers.name, answers.id, answers.email, answers.gitHub);
-            employeeArray.push(addEngineer);
+            engineerArray.push(addEngineer);
             firstQuestions()
         })
     };
@@ -174,7 +176,7 @@ const managerQuestions = () => {
     ])
     .then((answers) => {
         const addIntern =  new Intern(answers.name, answers.id, answers.email, answers.school);
-        employeeArray.push(addIntern);
+        internArray.push(addIntern);
         firstQuestions()
     })
 };
@@ -200,7 +202,9 @@ const firstQuestions = () => {
             internQuestions()
         }
         else if (answers.emplRole === "I'm Done"){
-            console.log(employeeArray);
+            console.log(managerArray);
+            console.log(engineerArray);
+            console.log(internArray);
 
         }
 
